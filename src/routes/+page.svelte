9694498bf4f75b16d1ef855ff2d866944230fe90
matteopolak <matteopolak@hotmail.com>
2023-03-05 00:29:33 -0500

    fix: hide creation button if logged in

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
-<script>
+<script lang="ts">
 	import Header from '../components/Header.svelte';
 	import { page } from '$app/stores';
 	import { Button, Heading, P } from 'flowbite-svelte';
 	import { browser } from '$app/environment';
 
-	let key = $page.url.searchParams.get('key');
+	let key: string | null = $page.url.searchParams.get('key');
 
 	if (browser) {
 		if (key) {
@@ -27,22 +27,24 @@
 		</P>
 	</span>
 
-	<div class="flex flex-row justify-center gap-2">
-		<Button href="/register" color="blue" size="xl" pill>
-			Create an account
-			<svg
-				aria-hidden="true"
-				class="ml-2 -mr-1 w-5 h-5"
-				fill="currentColor"
-				viewBox="0 0 20 20"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path
-					fill-rule="evenodd"
-					d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-					clip-rule="evenodd"
-				/>
-			</svg>
-		</Button>
-	</div>
+	{#if key === null}
+		<div class="flex flex-row justify-center gap-2">
+			<Button href="/register" color="blue" size="xl" pill>
+				Create an account
+				<svg
+					aria-hidden="true"
+					class="ml-2 -mr-1 w-5 h-5"
+					fill="currentColor"
+					viewBox="0 0 20 20"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+						clip-rule="evenodd"
+					/>
+				</svg>
+			</Button>
+		</div>
+	{/if}
 </div>
