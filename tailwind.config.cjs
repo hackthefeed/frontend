da90c2ebe1d5ce8198f10550c933676060e79dce
matteopolak <matteopolak@hotmail.com>
2023-04-06 21:57:57 -0400

    fix: prose width

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,7 +9,15 @@ const config = {
 				"10xl": "12rem",
 			},
 		},
+		typography: {
+			DEFAULT: {
+				css: {
+					maxWidth: 'full',
+				}
+			}
+		}
 	},
+},
 	plugins: [
 		require('@tailwindcss/line-clamp'),
 		require('@tailwindcss/typography'),
@@ -20,27 +28,27 @@ const config = {
 			{
 				'light': {
 					...require('daisyui/src/colors/themes')['[data-theme=light]'],
-					'.bg-gradient-dots': {
-						'background-image': 'radial-gradient(circle at 1px 1px, rgba(0, 0, 0, 0.2) 1px, transparent 0);'
-					}
+			'.bg-gradient-dots': {
+				'background-image': 'radial-gradient(circle at 1px 1px, rgba(0, 0, 0, 0.2) 1px, transparent 0);'
+			}
 				},
 			},
-			{
-				'dark': {
+{
+	'dark': {
 					...require('daisyui/src/colors/themes')['[data-theme=dark]'],
-					'.bg-gradient-dots': {
-						'background-image': 'radial-gradient(circle at 1px 1px, rgba(70, 70, 70, 0.7) 1px, transparent 0);'
-					}
-				},
-			},
-			{
-				'synthwave': {
+			'.bg-gradient-dots': {
+			'background-image': 'radial-gradient(circle at 1px 1px, rgba(70, 70, 70, 0.7) 1px, transparent 0);'
+		}
+	},
+},
+{
+	'synthwave': {
 					...require('daisyui/src/colors/themes')['[data-theme=synthwave]'],
-					'.bg-gradient-dots': {
-						'background-image': 'radial-gradient(circle at 1px 1px, rgba(70, 70, 70, 0.7) 1px, transparent 0);'
-					}
-				},
-			},
+			'.bg-gradient-dots': {
+			'background-image': 'radial-gradient(circle at 1px 1px, rgba(70, 70, 70, 0.7) 1px, transparent 0);'
+		}
+	},
+},
 		]
 	}
 };
