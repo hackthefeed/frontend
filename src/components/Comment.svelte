984862002f6f83cae268e113075774895e9b173c
matteopolak <matteopolak@hotmail.com>
2023-04-08 13:47:16 -0400

    feat: complete comments overhaul

--- src/components/Comment.svelte
+++ src/components/Comment.svelte
@@ -72,22 +72,30 @@
 	</label>
 </label>
 
-<div class="flex flex-col gap-1">
-	<span>
-		<h1 class="text-lg font-bold text-primary inline">
-			{comment.author.displayName || comment.author.username}
-		</h1>
-		<Time timestamp={comment.createdAt} relative class="text-sm inline" />
-	</span>
+<div class="flex flex-row">
+	<label
+		for="new-reply-modal-{comment.id}"
+		class="btn btn-ghost w-12 h-12 p-0 mr-2"
+	>
+		<svg class="fill-current w-6 h-6" viewBox="0 0 24 24">
+			<path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
+		</svg>
+	</label>
 
-	<p class="text-lg">{comment.content}</p>
-	<label for="new-reply-modal-{comment.id}" class="btn btn-ghost">Reply</label>
+	<div class="flex flex-col gap-1">
+		<span>
+			<h1 class="text-lg font-bold text-primary inline">
+				{comment.author.displayName || comment.author.username}
+			</h1>
+			<Time timestamp={comment.createdAt} relative class="text-sm inline" />
+		</span>
 
-	{#if comment.children?.length}
-		<div style="margin-left: {(depth + 1) * 1.5}rem">
+		<p class="text-lg">{comment.content}</p>
+
+		{#if comment.children?.length}
 			{#each comment.children as child}
-				<svelte:self comment={child} depth={depth + 1} />
+				<svelte:self comment={child} depth={depth + 1} {postId} />
 			{/each}
-		</div>
-	{/if}
+		{/if}
+	</div>
 </div>
